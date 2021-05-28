1df44df76108689a2d4481ec39050ad0e4dc601d
Francisco Varela Cid <fvarelacid@gmail.com>
2021-05-28 15:48:55 +0100

    Update github.py

---
+++ github.py
@@ -0,0 +1,9 @@
+#This is a calculator that only multiplies
+
+def calculator(x, y):
+    x = input("First digit: ")
+    y = input("Second digit: ")
+    mult = x * y
+    return print(mult)
+
+calculator()
